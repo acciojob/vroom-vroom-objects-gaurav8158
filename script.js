d6adb73820c93f37c15def846fceb49c3ebdb7e9
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-12-15 17:11:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+function Car(make, model) {
+make = this.make;
+	model = this.model;
+}
+Car.prototype.getMakeModel = function () {
+  return this.make + ' ' + this.model;
+};
 
+function SportsCar(make, model, topSpeed) {
+	car.call(this,topSpeed)
+this.topSpeed = topSpeed;
+}
+SportsCar.prototype=Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Add a method to the SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
